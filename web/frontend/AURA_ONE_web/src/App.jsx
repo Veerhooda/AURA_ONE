bd0bc753768a058bb78d0a3781b5d8d70f16465e
Priyanshu Kumar <priyanshu19jul@gmail.com>
2025-12-12 20:40:18 +0530

    sab kuch dubara start

--- web/frontend/AURA_ONE_web/src/App.jsx
+++ web/frontend/AURA_ONE_web/src/App.jsx
@@ -1,36 +1,34 @@
-import { useState, useEffect } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import Loader from './components/loader/Loader'
-import Home from './pages/home/home'
-import Login from './pages/auth/login'
-import Navbar from './components/navbar/navbar'
-import Profile from './pages/profile/profile'
 
 function App() {
-  const [loading, setLoading] = useState(true)
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false)
-    }, 3000)
-
-    return () => clearTimeout(timer)
-  }, [])
-
-  if (loading) {
-    return <Loader />
-  }
+  const [count, setCount] = useState(0)
 
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
 
